cbf20ac316ba455235c365b84caa61e4237cdbf4
lennykaris <glnkariuki@gmail.com>
2024-05-28 12:14:21 +0300

    toi

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,22 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
 
-        document.getElementById('view-options-btn').addEventListener('click', function(event) {
-            event.preventDefault();
-            var colorInput = document.getElementById('color-input');
-            if (colorInput.style.display === 'none' || colorInput.style.display === '') {
-                colorInput.style.display = 'block';
-            } else {
-                colorInput.style.display = 'none';
-            }
-        })
+document.getElementById('view-options-btn').addEventListener('click', function(event) {
+    event.preventDefault();
+    var colorInput = document.getElementById('color-input');
+    if (colorInput.style.display === 'none' || colorInput.style.display === '') {
+        colorInput.style.display = 'block';
+    } else {
+        colorInput.style.display = 'none';
+    }
+});
+
+document.getElementById('submit-color-btn').addEventListener('click', function(event) {
+    event.preventDefault();
+    var colorValue = document.getElementById('color').value.toLowerCase();
+    if (colorValue === 'black' || colorValue === 'white') {
+        alert('Valid color: ' + colorValue);
+    } else {
+        alert('Please enter either "black" or "white".');
+    }
+});
